545464864cb42582f45ea51be79b2f82e19f7863
yy99992016 <535392529@qq.com>
2016-11-11 17:34:59 +0800

    lab6

---
+++ src/gx/polyvalTest.java
@@ -0,0 +1,18 @@
+package gx;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import gx.polyval;
+
+public class polyvalTest {
+
+	@Test
+	public void testExpression() {
+		polyval  np = new polyval();
+		assertEquals(false,np.expression("8#b/c"));
+
+	}
+
+}
